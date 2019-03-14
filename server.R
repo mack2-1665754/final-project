b02ebd6bca6dc77681ef7bcd81579798df2f6dc1
MACKENZIE HUTCHISON <mack2@uw.edu>
2019-03-13 22:12:02 -0700

    finish project

--- server.R
+++ server.R
@@ -6,6 +6,8 @@ library(plotly)
 library(devtools)
 library(stringr)
 library(leaflet)
+library(tidyr)
+
 
 police_killings <- read.csv("data/police_killings.csv", stringsAsFactors = F)
 
@@ -14,17 +16,19 @@ shinyServer(function(input, output) {
   output$death_cause <- renderPlotly({
     death_data <- police_killings %>%
       select(cause, armed, state, name, age) %>%
-      filter(state == input$select_state)%>%
+      filter(state == input$select_state) %>%
       group_by(cause, armed, state) %>%
       count()
-    title <- paste0("Cause of Death and Arming of the Victim in ", input$select_state)
+    title <- paste0("Cause of Death and Arming of the Victim in ",
+                    input$select_state)
     death_plot <- plot_ly(
       data = death_data,
       x = ~cause,
       y = ~n,
       color = ~armed,
       type = "bar",
-      alpha = .7) %>%
+      alpha = .7
+    ) %>%
       layout(
         title = title,
         xaxis = list(title = "Cause of Death"),
@@ -34,30 +38,25 @@ shinyServer(function(input, output) {
         barmode = "stack"
       )
     death_plot
-  })
-  output$message <- renderText({
-    death_sum <- death_data %>%
-      filter(state == input$select_state)%>%
-      group_by(state) %>%
-      summarise(state_total = sum(n))
-    msg <- paste("In ", input$select_state, "there were a total of ", death_sum$state_total, " deaths by a police officer.")
   }) # Ali's Code
   output$police_map <- renderLeaflet({
     palette1 <- colorFactor(
-      palette = "Dark2", 
+      palette = "Dark2",
       domain = police_killings[[input$analysis]]
     )
-    leaflet(data = police_killings) %>% 
-      addProviderTiles("OpenStreetMap.Mapnik") %>% 
+    leaflet(data = police_killings) %>%
+      addProviderTiles("OpenStreetMap.Mapnik") %>%
       addCircleMarkers(
         lat = ~latitude,
         lng = ~longitude,
-        label = ~paste0("Victim: ", name, ". ", raceethnicity, " ", gender, " age ", age, ". ", "Cause of death: ", cause, "."),
-        color = ~palette1(police_killings[[input$analysis]]),
+        label = ~ paste0("Victim: ", name, ". ", raceethnicity, "
+                         ", gender, " age ", age, ". ", "Cause of death: ",
+                         cause, "."),
+        color = ~ palette1(police_killings[[input$analysis]]),
         fillOpacity = .5,
         radius = 3,
         stroke = F
-      ) %>% 
+      ) %>%
       addLegend(
         position = "bottomleft",
         title = input$analysis,
@@ -67,53 +66,63 @@ shinyServer(function(input, output) {
       )
   })
   output$police_table <- renderTable({
-    table1 <- police_killings %>% 
-      group_by(police_killings[[input$analysis]]) %>% 
+    table1 <- police_killings %>%
+      group_by(police_killings[[input$analysis]]) %>%
       count()
     colnames(table1) <- c(input$analysis, "Number of Victims")
     table1
-  }) #Nick's Code
+  }) # Nick's Code
   output$classplot <- renderLeaflet({
     color_palette <- colorFactor(
       palette = "RdYlBu",
       domain = police_killings$raceethnicity
     )
-    
-    if(input$class == "All"){
-      msg <- paste0("In the United States in 2015, there were ", nrow(police_killings), " police killings")
-    } else if(input$class == "poor"){
-      police_killings <- police_killings %>% 
+
+    if (input$class == "All") {
+      msg <- paste0("In the United States in 2015, there were ",
+                    nrow(police_killings), " police killings")
+    } else if (input$class == "poor") {
+      police_killings <- police_killings %>%
         filter(h_income < 30000)
-      msg <- paste0("In the United States in 2015, there were ", nrow(police_killings), 
-                    " police killings within the poverished class")
-    } else if(input$class == "low"){
-      police_killings <- police_killings %>% 
+      msg <- paste0(
+        "In the United States in 2015, there were ", nrow(police_killings),
+        " police killings within the poverished class"
+      )
+    } else if (input$class == "low") {
+      police_killings <- police_killings %>%
         filter(h_income >= 30000 & h_income < 50000)
-      msg <- paste0("In the United States in 2015, there were ", nrow(police_killings), 
-                    " police killings within the lower-middle class")
-    } else if(input$class == "middle"){
-      police_killings <- police_killings %>% 
+      msg <- paste0(
+        "In the United States in 2015, there were ", nrow(police_killings),
+        " police killings within the lower-middle class"
+      )
+    } else if (input$class == "middle") {
+      police_killings <- police_killings %>%
         filter(h_income >= 50000 & h_income < 100000)
-      msg <- paste0("In the United States in 2015, there were ", nrow(police_killings), 
-                    " police killings within the middle class")
-    } else if(input$class == "upper"){
-      police_killings <- police_killings %>% 
+      msg <- paste0(
+        "In the United States in 2015, there were ", nrow(police_killings),
+        " police killings within the middle class"
+      )
+    } else if (input$class == "upper") {
+      police_killings <- police_killings %>%
         filter(h_income >= 100000 & h_income < 350000)
-      msg <- paste0("In the United States in 2015, there were ", nrow(police_killings), 
-                    " police killings within the upper-middle class")
+      msg <- paste0(
+        "In the United States in 2015, there were ", nrow(police_killings),
+        " police killings within the upper-middle class"
+      )
     }
-    
-    leaflet(data = police_killings) %>% 
-      addProviderTiles("OpenStreetMap.Mapnik") %>% 
+
+    leaflet(data = police_killings) %>%
+      addProviderTiles("OpenStreetMap.Mapnik") %>%
       addCircleMarkers(
         lat = ~latitude,
         lng = ~longitude,
-        label = ~paste(name, "," , age, "," , gender, "," , "House Income:", h_income),
-        color = ~color_palette(police_killings$raceethnicity),
+        label = ~ paste(name, ",", age, ",", gender, ",",
+                        "House Income:", h_income),
+        color = ~ color_palette(police_killings$raceethnicity),
         fillOpacity = .5,
         radius = 3,
         stroke = F
-      ) %>% 
+      ) %>%
       addLegend(
         position = "bottomright",
         title = "Race",
@@ -121,8 +130,83 @@ shinyServer(function(input, output) {
         values = police_killings$raceethnicity,
         opacity = 1
       )
+  }) # Mackenzie's code
+  output$poverty_vs_police_brutality <- renderPlotly({
+    available_races <- c("White", "Black", "Hispanic/Latino")
+    # Calculate the total population for each race, out of the communities
+    #where police killings have occurred
+    pops_by_race <- police_killings %>%
+      select(pop, share_white, share_black, share_hispanic) %>%
+      mutate(
+        "white_pop" = as.numeric(pop) * (as.numeric(share_white) / 100),
+        "hispanic_pop" = as.numeric(pop) * (as.numeric(share_black) / 100),
+        "black_pop" = as.numeric(pop) * (as.numeric(share_hispanic) / 100)
+      ) %>%
+      summarise(
+        total_white_pop = sum(white_pop, na.rm = TRUE),
+        total_black_pop = sum(black_pop, na.rm = TRUE),
+        total_hispanic_pop = sum(hispanic_pop, na.rm = TRUE)
+      )
+    # Divide the number of each subgroup by the appropriate racial
+    # population, to calculate what percentage of each racial population
+    # that subgroup represents.
+    # For example, what perce nt of the white population is killed by the
+    # police without being armed?
+
+    # Select the races for which we have available 'population share' data
+    # i.e. white, black, and hispanic, because there are
+    # share_white, share_black, and share_hispanic columns
+    perc_total_pop <- police_killings %>%
+      filter(raceethnicity %in% available_races) %>%
+      group_by(raceethnicity, armed) %>%
+      count() %>%
+      group_by(armed)
+
+    # Take the races in the 'raceethnicity' column, and make them
+    # columns containing the count of victims, according to each type of arming
+    spread_p_total_pop <- spread(
+      perc_total_pop,
+      key = raceethnicity,
+      value = n
+    )
+    # Rename the Hispanic/Latino coumn to Hispanic, so R does not think
+    # we are trying to treat Hispanic and Latino as two variables, and
+    # divide them by one another
+    names(spread_p_total_pop)[3] <- "Hispanic"
+    # Take the 'spread' version of the data, and use the population
+    # totals by race to calculate the percent of each racial population
+    # that was killed, according to each type of arm they had
+    spread_p_total_pop <- spread_p_total_pop %>%
+      mutate(
+        "Black" = Black / pops_by_race$total_black_pop,
+        "White" = White / pops_by_race$total_white_pop,
+        "Hispanic" = Hispanic / pops_by_race$total_hispanic_pop
+      ) %>%
+      select(armed, White, Black, Hispanic)
+    # Take the data with the percentage of populations calculated,
+    # and and use gather to make a coumn for the firearm type, racial group
+    # and percentage of the total population of a given race,
+    # which that subgroup of victims represents.
+    final_df <- gather(
+      spread_p_total_pop,
+      key = racial_group,
+      value = perc_of_pop,
+      -armed
+    ) %>%
+      filter(racial_group %in% input$victim_race) %>%
+      filter(armed %in% input$armed_type)
+
+    # Create a plot which visually represents this
+    plot <- ggplot(final_df) +
+      geom_col(mapping = aes(x = racial_group, y = perc_of_pop, fill = armed),
+               position = "dodge") +
+      labs(
+        title = "Percent of Each Racial Population Killed by the Police",
+        x = "Victim Racial group",
+        y = "% of the Each Racial Population Killed by Police",
+        fill = "How the Victim was Armed"
+      )
+    plot <- ggplotly(plot)
+    plot
   })
 })
-
-
-
--- ui.R
+++ ui.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(plotly)
 library(leaflet)
+library(tidyr)
 
 # Read in Data
 police_killings <- read.csv("data/police_killings.csv", stringsAsFactors = F)
@@ -12,16 +13,27 @@ police_killings <- read.csv("data/police_killings.csv", stringsAsFactors = F)
 # Page one: Intro Page
 page_3 <- shinyUI(navbarPage(
   "Police Killings",
-  tabPanel("Overview",
-           titlePanel("Overview of Police Killings in 2015"),
-  
-  tags$head(
-    tags$style(HTML("
+  tabPanel(
+    "Overview",
+    titlePanel("Overview of Police Killings in 2015"),
+
+    tags$head(
+      tags$style(HTML("
                     h1 {
                     color: #191970;
                     text-align: center;
                     font-style: bold;
                     font-family: georgia;
+                    }
+                    h3 {
+                    color: #191970;
+                    text-align: center;
+                    font-size: 20px;
+                    font-style: italic;
+                    font-family: georgia;
+                    }
+                    url {
+                    text-aling: center;
                     }
                     h2 {
                     color: #191970;
@@ -45,140 +57,206 @@ page_3 <- shinyUI(navbarPage(
                     display: block;
                     margin-left: auto;
                     margin-right: auto;
-                    
                     }
                     
                     "))
     ),
-  
-  
-  # A first-level header (`h1()`) with the content "First Shiny Website"
-  h1("Police Killing Americans in 2015"),
-  h2("Data from the Github database FiveThirtyEight"),
-  
-  # An image (`img()`) with no content but the `src` attribute of the url:
-  img("", src = "https://dailygazette.com/sites/default/files/styles/gallery_image/public/2017-06/ms12prb_0.jpg?itok=ODRf8lW1"),
-  
-  
-  
-  p("As we explore the data, we seek to answer questions about the story where
-    the police have killed Americans in 2015. The questions we seek are", 
-    strong("who"), "are the victims of police killings in regards to their 
-    location, race, and gender, as well as their cause of death?", strong("Which"), "states 
-    have the highest cause of death and were the victims killed in 
-    stone cold murder or in self-defense as a result of armed?", strong("Which"),
-    "social class has the most death by police? Lastly,", strong("which"), 
-    "race has the largest percentage of their population killed by cops?")
-  ),
-#Page 3: Ali's Page
 
-  tabPanel("Map of Police Killings in 2015",
-  titlePanel("Map of Police Killings in 2015"),
-  h5("This map shows victims of police killings in 2015. Below, you can colorize the data through either gender, 
-     race/ethnicity or cause of death. Hovering over data points on the map will display the victims name, ethnicity, gender,
-     age and cause of death."),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "analysis",
-        label = "Select a unit of analysis",
-        choices = c("gender", "race" = "raceethnicity", "cause")
+
+    # A first-level header (`h1()`) with the content "First Shiny Website"
+    h1("Police Killing Americans in 2015"),
+    h2("Data from the Github database FiveThirtyEight"),
+    url <- a("See Our Data Source", href = "
+https://github.com/fivethirtyeight/data/blob/master/police-killings/police_killings.csv
+    "),
+
+    # An image (`img()`) with no content but the `src` attribute of the url:
+    img("", src = "https://static01.nyt.com/images/2012/08/13/nyregion/SHOOT/SHOOT-jumbo.jpg"),
+
+
+
+    p(
+    "As we explore the data, we seek to answer questions about the story where
+    the police have killed Americans in 2015. The questions we seek are",
+      strong("who"), "are the victims of police killings in regards to their 
+    location, race, and gender, as well as their cause of death?",
+    strong("Which"), "states have the highest cause of death and were the
+    victims killed in stone cold murder or in self-defense as a result of
+    armed?", strong("Which"), "social class has the most death by police?
+    Lastly,", strong("which"), "race has the largest percentage of their
+    population killed by cops?"
+    )
+  ),
+  # Page 3: Ali's Page
+
+  tabPanel(
+    "Map of Police Killings in 2015",
+    h1(strong("Map of Police Killings in 2015")),
+    p("This map shows victims of police killings in 2015. Below, you can
+      colorize the data through either gender, race/ethnicity or cause of
+      death. Hovering over data points on the map will display the victims
+      name, ethnicity, gender, age and cause of death."),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          inputId = "analysis",
+          label = "Select a unit of analysis",
+          choices = c("gender", "race" = "raceethnicity", "cause")
+        )
+      ),
+      mainPanel(
+        leafletOutput(outputId = "police_map"),
+        tableOutput((outputId <- "police_table"))
       )
-    ),
-    mainPanel(
-      leafletOutput(outputId = "police_map"),
-      tableOutput((outputId = "police_table")))
-  )
-    ),
-
-
-
-
-#Page 2: Ken's Page
-
-#Page 4: Nick's Page
-tabPanel("Deaths in Each Economic Class",
-titlePanel("Deaths by Economic Class"),
-sidebarLayout(
-  sidebarPanel(
-    selectInput("class",
-                "Which economic class?",
-                choices = c(
-                  "All",
-                  "Poverished Class [$0-30,000)" = "poor",
-                  "Lower-Middle Class [$30,000-50,000)" = "low",
-                  "Middle Class [$50,000-100,000)" = "middle",
-                  "Upper-Middle Class [$100,000-350,000)" = "upper"
-                ),
-                selected = "All"
-    ),
-    p("Note: There were no police killings within the Upper Class ($350,000 & more)")
+    )
   ),
-  mainPanel(
-    leafletOutput("classplot")
-  )
-)
-),
 
-#Page 5: Cass's Page
+
+
+
+  # Page 2: Mackenzie's Page
+  tabPanel(
+    "Percent of Each Race Killed",
+    h1(strong("What Percent of Each Racial Population has Been Killed by
+              the Police?")),
+    h3(strong("The percentages are sorted by how the victims were armed.")),
+    p("Each bar represents a percentage of the population of the race in which
+      it is classified. Percentages, rather than a raw count of victims
+      by race, "),
+    p("allow races to be compared to one another, accounting for the fact that
+      different racial populations are different sizes. 
+      For example, the bar for "),
+    p(strong("victims with firearms"), "in the", strong("Black"), 
+      "category shows
+      the percentage of the ", strong("total black population"),
+      "(across the documented communities)"),
+    p("that were killed by police while armed with a firearm. Based on their
+      selections, the user will be presented information about which
+      races are"),
+    p(strong("more affected"), "than others by police killings."),
+    sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput(
+          inputId = "armed_type",
+          label = strong("Select how the Victim was Armed:"),
+          choices = c("Disputed", "Firearm", "Knife", "No",
+                      "Non-lethal firearm",
+                      "Other", "Vehicle"),
+          selected = c("Disputed", "Firearm", "Knife", "No",
+                       "Non-lethal firearm", 
+                       "Other", "Vehicle")
+        ),
+        checkboxGroupInput(
+          inputId = "victim_race",
+          label = strong("Racial Population the Victim Belongs to:"),
+          choices = c("White", "Black", "Hispanic"),
+          selected = c("White", "Black", "Hispanic")
+        )
+      ),
+      mainPanel(
+        plotlyOutput(outputId = "poverty_vs_police_brutality"),
+        p("Keep in mind, the percentages on the y-axis are ",
+          strong("very small"), 
+          ", because a very small percent of the full population is",
+          em("actually", strong("killed")), strong("by police.")),
+        p("The percentage measure is for the sake of the ",
+          strong("impact on of police killing on racial groups"),
+          "being compared,",
+          strong("across races."))
+      )
+    )
+  ),
+  # Page 4: Nick's Page
+  tabPanel(
+    "Deaths in Each Economic Class",
+    h1(strong("Deaths by Economic Class")),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("class",
+          "Which economic class?",
+          choices = c(
+            "All",
+            "Poverished Class [$0-30,000)" = "poor",
+            "Lower-Middle Class [$30,000-50,000)" = "low",
+            "Middle Class [$50,000-100,000)" = "middle",
+            "Upper-Middle Class [$100,000-350,000)" = "upper"
+          ),
+          selected = "All"
+        ),
+        p("Note: There were no police killings within the Upper
+          Class ($350,000 & more)")
+      ),
+      mainPanel(
+        leafletOutput("classplot")
+      )
+    )
+  ),
+
+  # Page 5: Cass's Page
   tabPanel(
     "Cause of Death",
-  titlePanel("Relationship Between Cause of Death and Arming of the Victim in 2015"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "select_state",
-        label = "Select Your State",
-        choices = c("Alabama" = "AL",
-                    "Alaska" = "AK",
-                    "Arizona" = "AZ",
-                    "Arkansas" = "AR",
-                    "California" = "CA",
-                    "Colorado" = "CO",
-                    "Connecticut" = "CT",
-                    "Delware" = "DE",
-                    "Washington D.C" = "DC",
-                    "Florida" = "FL",
-                    "Georgia" = "GA",
-                    "Hawaii" = "HI",
-                    "Idaho" = "ID",
-                    "Illinois" = "IL",
-                    "Indiana" = "IN",
-                    "Iowa" = "IA",
-                    "Kansas" = "KS",
-                    "Kentucky" = 'KY',
-                    "Louisiana" = "LA",
-                    "Maine" = "ME",
-                    "Mchigan" = "MI",
-                    "Minnesota" = "MN",
-                    "Mississippi" = "MS",
-                    "Missouri" = "MO",
-                    "Montana" = "MT",
-                    "Nebraska" = "NE",
-                    "Nevada" = "NV",
-                    "New Hampshire" = "NH",
-                    "New Jersey" = "NJ",
-                    "New Mexico" = "NM",
-                    "New York" = "NY",
-                    "North Carolina" = "NC",
-                    "Ohio" = "OH",
-                    "Oklahoma" = "OK",
-                    "Oregon" = "OR",
-                    "Pennsylvania" = "PA",
-                    "South Carolina" = "SC",
-                    "Tennessee" = "TN",
-                    "Texas" = "TX",
-                    "Utah" = "UT",
-                    "Virginia" = "VA",
-                    "Washington" = "WA",
-                    "West Virgina" = "WV",
-                    "Wisconsin" = "WI")),
-      textOutput("message"),
-      h5("In 2015, there were 467 deaths that were caused by a police officer. 
-     On the side, you can analyze the incidents that happened in each state. 
-        You will also be able to see if the victim was armed during the incident and what they were armed with.")
-    ),
-    mainPanel(
-      plotlyOutput("death_cause"))
-  ))))
-
+    h1(strong("Relationship Between Cause of Death and Arming of the
+              Victim in 2015")),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          inputId = "select_state",
+          label = "Select Your State",
+          choices = c(
+            "Alabama" = "AL",
+            "Alaska" = "AK",
+            "Arizona" = "AZ",
+            "Arkansas" = "AR",
+            "California" = "CA",
+            "Colorado" = "CO",
+            "Connecticut" = "CT",
+            "Delware" = "DE",
+            "Washington D.C" = "DC",
+            "Florida" = "FL",
+            "Georgia" = "GA",
+            "Hawaii" = "HI",
+            "Idaho" = "ID",
+            "Illinois" = "IL",
+            "Indiana" = "IN",
+            "Iowa" = "IA",
+            "Kansas" = "KS",
+            "Kentucky" = "KY",
+            "Louisiana" = "LA",
+            "Maine" = "ME",
+            "Mchigan" = "MI",
+            "Minnesota" = "MN",
+            "Mississippi" = "MS",
+            "Missouri" = "MO",
+            "Montana" = "MT",
+            "Nebraska" = "NE",
+            "Nevada" = "NV",
+            "New Hampshire" = "NH",
+            "New Jersey" = "NJ",
+            "New Mexico" = "NM",
+            "New York" = "NY",
+            "North Carolina" = "NC",
+            "Ohio" = "OH",
+            "Oklahoma" = "OK",
+            "Oregon" = "OR",
+            "Pennsylvania" = "PA",
+            "South Carolina" = "SC",
+            "Tennessee" = "TN",
+            "Texas" = "TX",
+            "Utah" = "UT",
+            "Virginia" = "VA",
+            "Washington" = "WA",
+            "West Virgina" = "WV",
+            "Wisconsin" = "WI"
+          )
+        ),
+        h5("In 2015, there were 467 deaths that were caused by a police
+            officer. On the side, you can analyze the incidents that
+            happened in each state. You will also be able to see if the
+          victim was armed during the incident and what they were armed with.")
+      ),
+      mainPanel(
+        plotlyOutput("death_cause")
+      )
+    )
+  )
+))
