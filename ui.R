896de7bc5c19d6cf8b018418ad3eef7169976819
Ken Wu <kenwu@Benjamins-MacBook-Air-3.local>
2019-03-13 20:04:32 -0700

    ken did it

--- ui.R
+++ ui.R
@@ -3,17 +3,75 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(leaflet)
 
 # Read in Data
 police_killings <- read.csv("police_killings.csv", stringsAsFactors = F)
 
 
 # Page one: Intro Page
-
-
-#Page 3: Ali's Page
 page_3 <- shinyUI(navbarPage(
   "Police Killings",
+  tabPanel("Overview",
+           titlePanel("Overview of Police Killings in 2015"),
+  
+  tags$head(
+    tags$style(HTML("
+                    h1 {
+                    color: #191970;
+                    text-align: center;
+                    font-style: bold;
+                    font-family: georgia;
+                    }
+                    h2 {
+                    color: #191970;
+                    text-align: center;
+                    font-size: 20px;
+                    font-style: italic;
+                    font-family: georgia;
+                    }
+                    p {
+                    color:#191970;
+                    text-align: center;
+                    font-size: 18px;
+                    padding-right: 50px;
+                    padding-left: 50px;
+                    font-family: georgia;
+                    }
+                    img {
+                    height: 500px;
+                    width: 600px;
+                    padding-bottom: 20px;
+                    display: block;
+                    margin-left: auto;
+                    margin-right: auto;
+                    
+                    }
+                    
+                    "))
+    ),
+  
+  
+  # A first-level header (`h1()`) with the content "First Shiny Website"
+  h1("Police Killing Americans in 2015"),
+  h2("Data from the Github database FiveThirtyEight"),
+  
+  # An image (`img()`) with no content but the `src` attribute of the url:
+  img("", src = "https://dailygazette.com/sites/default/files/styles/gallery_image/public/2017-06/ms12prb_0.jpg?itok=ODRf8lW1"),
+  
+  
+  
+  p("As we explore the data, we seek to answer questions about the story where
+    the police have killed Americans in 2015. The questions we seek are", 
+    strong("who"), "are the victims of police killings in regards to their 
+    location, race, and gender, as well as their cause of death?", strong("Which"), "states 
+    have the highest cause of death and were the victims killed in 
+    stone cold murder or in self-defense as a result of armed?", strong("Which"),
+    "social class has the most death by police? Lastly,", strong("which"), 
+    "race has the largest percentage of their population killed by cops?")
+  ),
+#Page 3: Ali's Page
+
   tabPanel("Map of Police Killings in 2015",
   titlePanel("Map of Police Killings in 2015"),
   h5("This map shows victims of police killings in 2015. Below, you can colorize the data through either gender, 
